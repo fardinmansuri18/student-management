efa35534e92ec94296c061b7ecd8f2dd0b406ae2
fardinmansuri18 <fardinmansuri4741@gmail.com>
2025-02-27 18:29:14 +0530

    Add files via upload

---
+++ School Student Project/Scripts/script.js
@@ -0,0 +1,9 @@
+// const student = document.querySelector('.btn-one')
+// student.addEventListener("click",function(){
+//     window.location.href = 'student.html';
+// })
+
+// const school = document.querySelector('.btn-two')
+// school.addEventListener("click",function(){
+//     window.location.href = 'school.html';
+// })
